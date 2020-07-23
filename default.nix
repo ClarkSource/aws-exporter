f6281712461b649d3467e880704a359ff0e98414
Aljosha Friemann <aljosha.friemann@clark.de>
2020-07-23 12:34:07 +0200

    nix derivation for local version

--- default.nix
+++ default.nix
@@ -1,21 +1,54 @@
 with import <nixpkgs> {};
 
+# let
+#   mach-nix = import (
+#     builtins.fetchGit {
+#       url = "https://github.com/DavHau/mach-nix/";
+#       ref = "2.1.0";
+#     }
+#   );
+# in
+# mach-nix.mkPythonShell {
+#   requirements = ''
+#     wheel
+#     setuptools
+#     prometheus_client
+#     boto3
+#     tox
+#     pre-commit
+#     nodeenv
+#   '';
+# }
+
 let
-  mach-nix = import (
-    builtins.fetchGit {
-      url = "https://github.com/DavHau/mach-nix/";
-      ref = "2.1.0";
-    }
-  );
-in
-mach-nix.mkPythonShell {
-  requirements = ''
-    wheel
+  aws_exporter = with python3.pkgs; buildPythonApplication rec {
+    pname = "aws_exporter";
+    version = "dev";
+
+    src = ./.;
+
+    # No tests included
+    doCheck = false;
+
+    nativeBuildInputs = [
+      git
+    ];
+
+    propagatedBuildInputs = [
+      boto3
+      prometheus_client
+    ];
+
+    meta = with lib; {
+      homepage = "https://github.com/clarksource/aws-exporter";
+      license = licenses.isc;
+    };
+  };
+in with python3.pkgs; pkgs.mkShell {
+  buildInputs = [
+    aws_exporter
     setuptools
-    prometheus_client
-    boto3
-    tox
     pre-commit
-    nodeenv
-  '';
+    tox
+  ];
 }
