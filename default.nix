d48bea9c176374a5306cacb023876865b584b049
Aljosha Friemann <aljosha.friemann@clark.de>
2020-09-22 17:10:25 +0200

    project setup

--- default.nix
+++ default.nix
@@ -1,25 +1,5 @@
 with import <nixpkgs> {};
 
-# let
-#   mach-nix = import (
-#     builtins.fetchGit {
-#       url = "https://github.com/DavHau/mach-nix/";
-#       ref = "2.1.0";
-#     }
-#   );
-# in
-# mach-nix.mkPythonShell {
-#   requirements = ''
-#     wheel
-#     setuptools
-#     prometheus_client
-#     boto3
-#     tox
-#     pre-commit
-#     nodeenv
-#   '';
-# }
-
 let
   aws_exporter = with python3.pkgs; buildPythonApplication rec {
     pname = "aws_exporter";
